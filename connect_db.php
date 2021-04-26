61e8faf4f1714e725347e54d339be319c2b611cd
DESKTOP-VLNC8AO\samur <f_samura@salemstate.edu>
2021-04-26 17:03:31 -0400

    Push the first version of my capstone project implementtation on github

---
+++ connect_db.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting (1);
+$con=mysqli_connect('localhost','root','', 's0360060_c521')or die("cannot connect to server");
+mysqli_select_db($con, "s0360060_c521")or die("cannot connect to database");
+
+?>
